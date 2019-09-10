a06e72aae3f80088d98593b0fa2a445b25935352
AsifAmin <asif_udr@yahoo.com>
2019-09-10 14:56:19 +1000

    circle ci  test

--- gulp/test-tasks/lint.js
+++ gulp/test-tasks/lint.js
@@ -1,15 +1,22 @@
 var gulpif = require('gulp-if');
-module.exports = function (gulp, plugins, config, fsPath, eslintReporter) {
+module.exports = function(gulp, plugins, config, fsPath, eslintReporter) {
   return () => {
-    return gulp.src(config.test.lint)
-       .once('data', function () { console.log('\x1b[1m', '   \n---linting tests---\n   '); })
-       .pipe(plugins.eslint({
-         configFile: 'tests/.eslintrc',
-       }))
+    return gulp
+      .src(config.test.lint)
+      .once('data', function() {
+        console.log('\x1b[1m', '   \n---linting tests---\n   ');
+      })
+      .pipe(
+        plugins.eslint({
+          configFile: '.eslintrc',
+        })
+      )
       .pipe(plugins.eslint.format())
       .pipe(gulpif(process.env.NODE_ENV === 'prod', plugins.eslint.failAfterError()))
-      .pipe(plugins.eslint.format(eslintReporter, function (results) {
-        fsPath.writeFile('tests/reports/eslint/report.html', results);
-      }));
+      .pipe(
+        plugins.eslint.format(eslintReporter, function(results) {
+          fsPath.writeFile('tests/reports/eslint/report.html', results);
+        })
+      );
   };
 };
