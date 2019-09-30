65e2cb8ee3a5bd286008904e16024d37179167fd
AsifAmin <asif_udr@yahoo.com>
2019-09-30 13:55:29 +1000

    test

--- e2e/__tests__/mobile-spec.js
+++ e2e/__tests__/mobile-spec.js
@@ -8,12 +8,12 @@ beforeAll(async () => {
     executablePath: ct.CHROME_PATH,
   });
   page = await browser.newPage();
+  await page.setViewport({ width: ct.BT_SM, height: 800 });
+  await page.goto(`${ct.APP_URL}/docs/components.html`, { waitUntil: 'networkidle0' });
 });
 
 describe('SWE Mobile Interactions', () => {
   test('Should display the menu clicking menu icon', async () => {
-    await page.setViewport({ width: ct.BT_SM, height: 800 });
-    await page.goto(`${ct.APP_URL}/docs/components.html`, { waitUntil: 'networkidle0' });
     expect(await page.evaluate("document.querySelector('#qg-site-nav').getAttribute('class')")).not.toMatch(/collapse show/);
     (await page.$('#qg-show-menu')).click();
     await page.waitFor(3000);
